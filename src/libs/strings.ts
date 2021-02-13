1f65d02627531516437cd54c43454d5ec9e49cfc
uihyeon.cha <uihyeon.cha@gmail.com>
2021-02-14 06:41:34 +0900

    텍스트 추가

---
+++ src/libs/strings.ts
@@ -0,0 +1,2 @@
+export const loremIpsum =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vulputate elit quis rhoncus cursus. Etiam pellentesque pellentesque gravida. Duis ac vehicula purus. Proin in dignissim nibh. Duis tincidunt erat id mollis euismod. Cras interdum in erat at placerat. Vivamus tincidunt cursus vestibulum.'
