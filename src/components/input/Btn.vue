c2ac861da3ac015c1c993aa7e05905ba32ccac87
uihyeon.cha <uihyeon.cha@gmail.com>
2021-02-14 12:45:56 +0900

    Btn 컴포넌트 추가

---
+++ src/components/input/Btn.vue
@@ -0,0 +1,59 @@
+<template>
+    <button class="ui-btn" :disabled="disabled" @click="onClick">
+        <slot />
+    </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Btn extends Vue {
+    @Prop({
+        type: Boolean,
+        default: false,
+    })
+    readonly disabled!: boolean
+
+    onClick() {
+        if (this.disabled) {
+            return
+        }
+
+        /**
+         * @event click
+         */
+        this.$emit('click')
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/variable.scss';
+
+.ui-btn {
+    display: inline-flex;
+    border: 0;
+    border-radius: 4px;
+    background: $color-orange;
+    color: rgba($color: $color-white, $alpha: 0.8);
+    box-sizing: border-box;
+    padding: 6px 8px;
+    line-height: 1;
+    cursor: pointer;
+
+    &:hover {
+        color: rgba($color: $color-white, $alpha: 1);
+        box-shadow: 0 0 4px rgba($color: $color-black, $alpha: 0.2);
+    }
+
+    &:focus {
+        outline: 0;
+    }
+
+    &:disabled {
+        cursor: not-allowed;
+        opacity: 0.4;
+    }
+}
+</style>
---
+++ src/stories/Btn.stories.ts
@@ -0,0 +1,44 @@
+import { Story, Meta } from '@storybook/vue'
+import { Controls } from '@/libs/storybook'
+import Btn from '@/components/input/Btn.vue'
+
+export default ((): Meta => ({
+    title: 'Components/Btn',
+    component: Btn,
+    argTypes: {
+        label: {
+            control: Controls.text(),
+        },
+        disabled: {
+            control: Controls.boolean(),
+            defaultValue: false,
+        },
+        onClick: {
+            action: 'clicked',
+        },
+    },
+}))()
+
+const Template: Story = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { Btn },
+    template: `
+        <Btn
+            :disabled="disabled"
+            @click="onClick"
+        >
+            {{ label }}
+        </Btn>
+    `,
+})
+
+export const Default = Template.bind({})
+Default.args = {
+    label: '버튼',
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+    label: '버튼',
+    disabled: true,
+}
